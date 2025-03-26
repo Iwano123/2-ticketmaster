fe5d65617983109345436c237980ffa65ac77336
Iwano123 <korkisiwan@gmail.com>
2025-03-26 18:42:31 +0100

    Fixar ihop lite sidor och kopplar ihop dem, samt flyttar models in till Client med så den kan dela modeller.

---
+++ TicketToCode.Client/Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketToCode.Core.Models;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public int EventId { get; set; }
+    public int UserId { get; set; }
+    public int NumberOfTickets { get; set; }
+    public DateTime BookingDate { get; set; }
+}
---
+++ TicketToCode.Client/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿namespace TicketToCode.Core.Models;
+public class Event
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "New event";
+    public string Description { get; set; } = string.Empty;
+    public EventType Type { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int MaxAttendees { get; set; }
+    public int AvailableTickets { get; set; }
+}
+
+public enum EventType
+{
+    Concert = 0,
+    Festival,
+    Theatre,
+    Other
+}
